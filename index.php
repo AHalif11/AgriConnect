15a6a5f99ac346c1992537bb7c007d55e750e631
AH Alif <ahasanhabibalif99@gmail.com>
2025-08-23 16:56:49 +0600

    dashboard header and banner design commplete

--- index.php
+++ index.php
@@ -1,4 +1,47 @@
 <?php
-echo "dashboard";
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="icon" href="images/logo1.png">
+    <title>AgriConnect</title>
+</head>
+<body>
+    <header style="background: url(images/main-slider.png) no-repeat center center; background-size: cover;">
+      <nav>
+        <div class="nav-logo">
+          <h3 class="nav-title"><span>Agri</span>Connect</h3>
+        </div>
+
+        <ul>
+          <li>
+            <a href=""><span>Home</span></a>
+          </li>
+          <li><a href="">about</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+      </nav>
+      <div class="banner">
+        <div class="banner-content">
+            <div class="banner-description">
+                <p class="banner-head-para">WELCOME TO Agricultural Products Rural Entrepreneurship Management System.</p>
+                <h1>AGRI-CONNECT HUB</h1>
+                <P class="banner-foot-para">
+                    Empowering Rural Dreams, Nurturing Agricultural Growth - AgriConnect Hub cultivates prosperity from the roots up
+                </P>
+            </div>
+            <div class="buy-sell-btn">
+                <button class="sell-btn btn">Sell here</button>
+                <button class="buy-btn btn">Buy here</button>
+                
+            </div>
+        </div>
+
+      </div>
+    </header>
+</body>
+</html>
