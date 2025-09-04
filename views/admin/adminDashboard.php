28051d035e05c19376fffc87953c90328b9d0d82
Abu Bakar Siddique <siddique0203@gmail.com>
2025-09-04 22:37:06 +0600

    adminDashboard structure and design done

--- views/admin/adminDashboard.php
+++ views/admin/adminDashboard.php
@@ -1,9 +1,9 @@
-<?php
+<?php /*
 session_start();
 if (!isset($_SESSION['user_id'], $_COOKIE['logged_in']) || ($_SESSION['user_type'] ?? '') !== 'Admin') {
     header("Location: ../../views/login.php");
     exit;
-}
+}*/
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,7 +14,7 @@ if (!isset($_SESSION['user_id'], $_COOKIE['logged_in']) || ($_SESSION['user_type
 <link rel="icon" href="../../public/images/logo1.png">
 <title>Admin Dashboard</title>
 </head>
-<body>
+<body style="background-color: rgba(169, 245, 162, 1);">
 <header>
 <nav>
     <div class="nav-logo"><h3 class="nav-title"><span>Agri</span>Connect</h3></div>
@@ -25,8 +25,26 @@ if (!isset($_SESSION['user_id'], $_COOKIE['logged_in']) || ($_SESSION['user_type
     </ul>
 </nav>
 </header>
-<main>
-    <h1>Welcome, Admin</h1>
+<main style="padding-top: 100px;">
+   <div id="admin_pannel">
+        <div id="admin_left_pannel">
+            <h3>Hello Admin!</h3>
+            <h4>You are the boss of the system, can manage anything.</h4><br><br>
+            <a href="">Consumer</a><br>
+            <a href="">Farmer</a><br>
+            <a href="">Product</a>
+        </div>
+        <div id="admin_right_pannel">
+            <h2>Operations</h2><hr style="border: 1px solid black; margin-top:-5px;">
+
+           
+            <a href="" style="margin-left:250px; margin-top: 20px;">ADD</a>
+            <a href="">DELETE</a>
+            <a href="">UPDATE</a><br><br>
+            <div id="admin_show_data"> Data add Koro</div>
+            
+        </div>
+    </div>
 </main>
     <footer class="footer">
         <p>&copy; 2025 AgriConnect. All Rights Reserved.</p>
