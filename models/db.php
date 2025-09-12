63c9b9ab9e5272eb07dd8c5de4e902f3c62feb8d
Abu Bakar Siddique <siddique0203@gmail.com>
2025-09-12 11:41:38 +0600

    ...

--- models/db.php
+++ models/db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username   = "root";
 $password   = "";
-$dbname     = "demowork";
+$dbname     = "agriconnect";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
